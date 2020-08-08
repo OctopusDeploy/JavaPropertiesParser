71a1123e046d43e3ffc0fa74a61ca7652d3ee983
Andrew Moore <andrew.moore@octopus.com>
2020-08-08 11:41:46 +1000

    Add utility for encoding keys and values.

---
+++ source/JavaPropertiesParser/Utils/Encode.cs
@@ -0,0 +1,50 @@
+﻿using System.Text;
+
+namespace JavaPropertiesParser.Utils
+{
+    public static class Encode
+    {
+        private static string Common(string input)
+        {
+            var output = input
+                .Replace("\\", "\\\\")
+                .Replace("\r", "\\r")
+                .Replace("\n", "\\n")
+                .Replace("\t", "\\t");
+            
+            var result = new StringBuilder();
+
+            foreach (var ch in output)
+            {
+                var isIso88591Compatible = ch >= 0x00 && ch < 0xFF;
+                if (isIso88591Compatible)
+                {
+                    result.Append(ch);
+                }
+                else
+                {
+                    var hex = ((int)ch).ToString("x4");
+                    result.Append($"\\u{hex}");
+                }
+            }
+            
+            return result.ToString();
+        }
+
+        public static string Key(string input)
+        {
+            return Common(input)
+                .Replace(" ", "\\ ")
+                .Replace(":", "\\:")
+                .Replace("=", "\\=");
+        }
+        
+        public static string Value(string input)
+        {
+            var output = Common(input);
+            return output.StartsWith(" ") 
+                ? "\\ " + output.Substring(1) 
+                : output;
+        }
+    }
+}
