378dae98481d899946aa17ed4d79d91a72c0f16a
Andrew Moore <andrew.moore@octopus.com>
2020-08-10 09:57:49 +1000

    Add a generic Mutate method to Document.

--- source/JavaPropertiesParser.Tests/DocumentFixture.cs
+++ source/JavaPropertiesParser.Tests/DocumentFixture.cs
@@ -1,5 +1,9 @@
-﻿using Assent;
+﻿using System;
+using System.Collections.Generic;
+using Assent;
+using JavaPropertiesParser.Expressions;
 using JavaPropertiesParser.Tests.TestUtils;
+using JavaPropertiesParser.Utils;
 using NUnit.Framework;
 
 namespace JavaPropertiesParser.Tests
@@ -9,16 +13,53 @@ namespace JavaPropertiesParser.Tests
         [Test]
         public void CanReplaceValuesWhilePreservingFormatting()
         {
+            var keyValues = new Dictionary<string, string>
+            {
+                { "KEY1", "The new value for key\n1" },
+                { "key\\4", "The new value for key 4" },
+                { "key5", "Key 5's\n\n\nnewvalue" },
+                { "notarealkey", "Should not be found" },
+                { "", "New value for the empty key" },
+                { "key15", "" }
+            };
+
+            ITopLevelExpression Mutator(ITopLevelExpression expr)
+            {
+                switch (expr)
+                {
+                    case null:
+                        throw new ArgumentNullException(nameof(expr));
+
+                    case KeyValuePairExpression pair:
+                        var key = pair.Key?.Text?.LogicalValue ?? "";
+                        if (keyValues.TryGetValue(key, out var value))
+                        {
+                            return new KeyValuePairExpression(
+                                pair.Key,
+                                pair.Separator,
+                                new ValueExpression(
+                                    new StringValue(
+                                        value,
+                                        Encode.Value(value)
+                                    )
+                                )
+                            );
+                        }
+                        else
+                        {
+                            return expr;
+                        }
+
+                    default:
+                        return expr;
+                }
+            }
+            
             var input = ResourceUtils.ReadEmbeddedResource("multiple-pairs-and-comments.properties");
-            var parsed = Parser.Parse(input)
-                .SetValueIfExists("KEY1", "The new value for key\n1")
-                .SetValueIfExists("key\\4", "The new value for key 4")
-                .SetValueIfExists("key5", "Key 5's\n\n\nnewvalue")
-                .SetValueIfExists("notarealkey", "Should not be found")
-                .SetValueIfExists("", "New value for the empty key")
-                .SetValueIfExists("key15", "");
-        
-            var actual = parsed.ToString();
+            var parsed = Parser.Parse(input);
+            var updated = parsed.Mutate(Mutator);
+
+            var actual = updated.ToString();
             this.Assent(actual, TestEnvironment.AssentConfiguration);
         }
     }
--- source/JavaPropertiesParser/Expressions/Document.cs
+++ source/JavaPropertiesParser/Expressions/Document.cs
@@ -1,7 +1,5 @@
-﻿using System.Collections.Generic;
-using System.ComponentModel;
+﻿using System;
 using System.Linq;
-using System.Text;
 using JavaPropertiesParser.Utils;
 
 namespace JavaPropertiesParser.Expressions
@@ -15,70 +13,13 @@ namespace JavaPropertiesParser.Expressions
 
         public ITopLevelExpression[] Expressions { get; }
 
-        public Document SetValueIfExists(string key, string value)
+        public Document Mutate(Func<ITopLevelExpression, ITopLevelExpression> mutator)
         {
-            var newExpressions = Expressions.Mutate(item =>
-            {
-                switch (item)
-                {
-                    case KeyValuePairExpression pair when (pair.Key?.Text?.LogicalValue ?? "") == key:
-                        return new KeyValuePairExpression(
-                            pair.Key,
-                            pair.Separator,
-                            new ValueExpression(
-                                new StringValue(
-                                    value,
-                                    EncodeValue(value)
-                                )
-                            )
-                        );
-
-                    default:
-                        return item;
-                }
-            });
-
-            return ReferenceEquals(newExpressions, Expressions)
-                ? this
-                : new Document(newExpressions);
-        }
-
-        private string EncodeValue(string logicalValue)
-        {
-            var encodedValue = logicalValue
-                .Replace("\r", "\\r")
-                .Replace("\n", "\\n")
-                .Replace("\t", "\\t");
-
-            if (encodedValue.StartsWith(" "))
-                encodedValue = "\\ " + encodedValue.Substring(1);
-
-            var result = new StringBuilder();
+            var mutatedExpressions = Expressions.Mutate(mutator);
+            if (ReferenceEquals(Expressions, mutatedExpressions))
+                return this;
             
-            foreach (var ch in encodedValue)
-            {
-                if (IsIso88591Compatible(ch))
-                {
-                    result.Append(ch);
-                }
-                else
-                {
-                    result.Append(EscapeChar(ch));
-                }
-            }
-            
-            return result.ToString();
-        }
-
-        private static bool IsIso88591Compatible(char ch)
-        {
-            return ch >= 0x00 && ch < 0xFF;
-        }
-        
-        private static string EscapeChar(char ch)
-        {
-            var hex = ((int)ch).ToString("x4");
-            return $"\\u{hex}";
+            return new Document(mutatedExpressions);
         }
 
         public override string ToString()
